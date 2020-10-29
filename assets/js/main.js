f08507bf15422dc6def1c5a25bcd0a1df61e4f8d
AtluriNikhil <natluri2001@gmail.com>
2020-10-29 12:10:11 +0530

    fixed bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   setTimeout(function(){
     $('body').addClass('loaded');
     $('h1').css('color','#222222');
-  }, 3000);
+  }, 1000);
   
 });
 
@@ -126,8 +126,8 @@ myform.submit(function(event){
 
   
   $('[data-toggle="counter-up"]').counterUp({
-    delay: 5,
-    time: 4000
+    delay: 2.5,
+    time: 2700
   });
 
 
